ed29a9a120c12534c7e18702deb3bca136401aba
RocketAPI <developer@rocketapi.io>
2022-11-20 22:32:21 +0300

    fix

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setuptools.setup(
     version="1.0.0",
     author="RocketAPI",
     author_email="developer@rocketapi.io",
-    description="RocketAPI Python Wrapper",
-    packages=["rocketapi"]
+    description="RocketAPI Python SDK",
+    packages=["rocketapi"],
+    url="https://github.com/rocketapi-io/rocketapi-python",
+    install_requires=["requests"]
 )
